75b35476edc639111c6d9650c15f0718403d8171
KreyooKidd <gabojanny@gmail.com>
2025-04-03 17:48:20 +0300

    html.index edited

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+function student(){
+    var name = "Gabo";
+    var sex = "Male";
+}
+
+var name;
+
+var myArray = new Array(5);
+
+myArray[0] = 'Name';
+myArray[1] = 1;
+myArray[2] = 1.88976456;
+myArray[3] = false;
+myArray[4] = Student();
+
+myArray.length = 10;
+
+var Car3 = ["Ford","Toyota","Honda"];
+var Car2 = new Array("Ford", "Toyota", "Honda");
+
+
+var arrayLeft = myArray.reverse;
